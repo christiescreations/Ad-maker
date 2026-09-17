fe4508c5f3b914872193835890ef5468579d3094
christiescreations <christiemathew10@gmail.com>
2026-09-17 19:32:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def find_face(image_path):
 
     # scale face coordinates back to original image size
     if scale != 1.0:
-        faces = [(int(x/scale), int(y/scale), int(w_/scale), int(h_/scale)) for
+            faces = [(int(x/scale), int(y/scale), int(w_/scale), int(h_/scale)) for (x, y, w_, h_) in faces]
 # ── TEXT EMOTION ──────────────────────────────────────────────────────────────
 def analyze_text_emotion(text):
     t = text.lower()
